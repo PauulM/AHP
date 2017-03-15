41421860576383dd5ac5c18291bd91b8a3d32439
PauulM <pawmat@student.agh.edu.pl>
2017-03-15 19:51:18 +0100

    15.03 19:50

--- src/XmlDataTest.java
+++ src/XmlDataTest.java
@@ -1,4 +1,7 @@
 import org.junit.Test;
+import org.w3c.dom.Node;
+
+import java.util.ArrayList;
 
 /**
  * Created by pawma on 13.03.2017.
@@ -9,6 +12,13 @@ public class XmlDataTest {
     public void test(){
         XmlData xmlData = new XmlData("example.xml");
         xmlData.getByTagAndPrintName("criterium");
+    }
+
+    @Test
+    public void getAllEqualLevelCriteriaToNodeListTest(){
+        XmlData xmlData = new XmlData("example.xml");
+        ArrayList<Node> list = xmlData.getAllEqualLevelCriteriaToNodeList(1);
+        xmlData.getPriorityVectorFromCriteriaNodeList(list);
     }
 
 }
